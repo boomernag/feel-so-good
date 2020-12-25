fadf55e10ace134a13f531908d5ce1f631d47f6c
boomernag <william.dahlberg.n@gmail.com>
2020-12-25 17:26:23 +0000

    fix toast success img error

--- checkout/views.py
+++ checkout/views.py
@@ -1,4 +1,6 @@
-from django.shortcuts import render, redirect, reverse, get_object_or_404, HttpResponse
+from django.shortcuts import (
+    render, redirect, reverse, get_object_or_404, HttpResponse
+)
 from django.views.decorators.http import require_POST
 from django.contrib import messages
 from django.conf import settings
@@ -77,8 +79,9 @@ def checkout(request):
                             order_line_item.save()
                 except Product.DoesNotExist:
                     messages.error(request, (
-                        "One of the products in your bag wasn't found in our database. "
-                        "Please call us for assistance!")
+                        """One of the products in your bag
+                        wasn"t found in our database.
+                        Please call us for assistance!""")
                     )
                     order.delete()
                     return redirect(reverse('view_bag'))
@@ -103,7 +106,8 @@ def checkout(request):
             currency=settings.STRIPE_CURRENCY,
         )
 
-        # Attempt to prefill the form with any info the user maintains in their profile
+        # Attempt to prefill the form with any
+        # info the user maintains in their profile
         if request.user.is_authenticated:
             try:
                 profile = UserProfile.objects.get(user=request.user)
