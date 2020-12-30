09c48d287f499e16dbf839b7cde6f36438fc0334
boomernag <william.dahlberg.n@gmail.com>
2020-12-30 22:13:09 +0000

    added models

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,25 @@
-from django.db import models
+from datetime import datetime
 
-# Create your models here.
+from django.db import models
+from django.urls import reverse
+from django.utils.translation import ugettext_lazy as _
+
+from autoslug.fields import AutoSlugField
+from ckeditor.fields import RichTextField
+
+class BlogPost(models.Model):
+    title = models.CharField(_('title'), max_length=255)
+    slug = AutoSlugField(_('slug'), populate_from='title', unique=True)
+    image = models.ImageField(_('image'), blank=True, null=True, upload_to='blog')
+    text = RichTextField(_('text'))
+    description = models.TextField(_('description'), blank=True, null=True)
+    published = models.BooleanField(_('published'), default=False)
+
+    created = models.DateTimeField(_('created'), auto_now_add=True)
+    modified = models.DateTimeField(_('modified'), auto_now=True)
+    pub_date = models.DateTimeField(_('publish date'), blank=True, null=True)
+
+    class Meta:
+        verbose_name = _('blog post')
+        verbose_name_plural = _('blog posts')
+        ordering = ['pub_date']
